b8e39f9ec18de1d7747c3599a5833a37a5cfee1d
swetasagar <swetasagar997@gmail.com>
2026-07-02 23:19:35 +0530

    Delete BridgeLabz-SIPP-Training-2026-core-java-practice/core-java-practice directory

--- BridgeLabz-SIPP-Training-2026-core-java-practice/core-java-practice/programing-elements/level1/feeDiscount.java
+++
@@ -1,9 +0,0 @@
-package programmingElements;
-
-public class feeDiscount {
-    public static void main(String[] args) {
-        int fees=125000;
-        float discount=10.0f;
-        System.out.println("The discount amount is INR "+ fees*0.10+" and final discounted fee is INR "+fees*0.90);
-    }
-}
--- BridgeLabz-SIPP-Training-2026-core-java-practice/core-java-practice/programing-elements/level1/penDistribution.java
+++
@@ -1,9 +0,0 @@
-package programmingElements;
-
-public class penDistribution {
-    public static void main(String[] args) {
-        int noPens=14;
-        int students=3;
-        System.out.println("The Pen Per Student is  "+ noPens/students+"  and the remaining pen not distributed is "+noPens%students);
-    }
-}
